467033c1b82e6de77fb93e867790f702ddd8191a
Hien <k212nguyen@gmail.com>
2019-03-23 08:48:28 +0700

    6

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use((req,res,next)=>{
     res.locals.success_message = req.flash('success_message')
     next();
 })
-mongoose.connect('mongodb://localhost:27017/singer1602',{
+mongoose.connect('mongodb://localhost:27017/nodejsdb',{
     useNewUrlParser:true,
     useCreateIndex:true,
 })
